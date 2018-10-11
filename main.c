e8271ace8ae97a4fddb032b320a4639848333432
yujeong <kreamno@gmail.com>
2018-10-11 15:48:19 +0900

    문자분류

--- main.c
+++ main.c
@@ -5,25 +5,33 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i;
-	int a; //절대값을 저장하는 변 
+	char c;
 	
-	printf("input an integer:");
-	scanf("%d",&i);
+	printf("input an char:");
+	scanf("%c",&c);
 	
 	
-	if(i>0)
+	if('A'<=c&&c<='Z')
 	{
-		a=i;
+	printf( "대문자입니다.");
 	}
 		
 	
-	else
+	else if('a'<=c&&c<='z')
 	{
-		a=-i;
+	printf("소문자입니다.");
 	}
 	
-	printf("절대값은 %i 입니다.",a);
+	else if('0'<=c&&c<='9')
+	{
+	printf("숫자 입니다.");
+	}
+	
+	else
+	{
+	printf("기타 글자입니다.");
+	}
+	
 	
 	return 0;
 }
